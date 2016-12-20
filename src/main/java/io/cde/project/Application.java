4e130ca978a3edf22a75a5a55784e92e1c80c74d
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-12-20 23:20:40 +0800

    新建项目程序的入口java文件

---
+++ src/main/java/io/cde/project/Application.java
@@ -0,0 +1,21 @@
+package io.cde.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liaofangcai.
+ * Created time 12/20/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * 程序入口.
+     * @param args this args
+     * @throws Exception this exception
+     */
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
